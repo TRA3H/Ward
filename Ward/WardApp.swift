4e7d1a20504d0a0606bdfd43d72dd0546893b81e
Cyrus Baybay <41849568+TRA3H@users.noreply.github.com>
2024-02-28 21:17:13 -0800

    Initial Commit

---
+++ Ward/WardApp.swift
@@ -0,0 +1,17 @@
+//
+//  WardApp.swift
+//  Ward
+//
+//  Created by Cyrus Baybay on 2/28/24.
+//
+
+import SwiftUI
+
+@main
+struct WardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
